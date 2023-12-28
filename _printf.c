c432bcdc5cdb528b81eca776dbe0f72c5b7eac62
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-28 15:17:05 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+void print_buff(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...)
+int _pintf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
@@ -25,19 +25,19 @@ int _printf(const char *format, ...)
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
+				print_buff(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
+			print_buff(buffer, &buff_ind);
+			flags = get_flg(format, &i);
+			width = get_with(format, &i, list);
+			precision = get_prec(format, &i, list);
+			size = get_siaz(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = handle_pint(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -45,7 +45,7 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	print_buff(buffer, &buff_ind);
 
 	va_end(list);
 
@@ -53,11 +53,11 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buff - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void print_buff(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
